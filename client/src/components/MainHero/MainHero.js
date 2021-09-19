0cb5bc0557d53af8163c717d85e6b692ab4ff2c7
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-19 02:05:17 -0700

    render mainCard

--- client/src/components/MainHero/MainHero.js
+++ client/src/components/MainHero/MainHero.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import "./MainHero.css"
 import animals from "../../assets/images"
-import {Container} from "reactstrap"
+import { Container } from "reactstrap"
+import { useQuery, gql } from "@apollo/client"
+
+
 
 function MainHero() {
+
+    const { loading, error, data } = useQuery(gql`
+        {
+            mainCards {
+                image
+                title
+            }
+        }
+    `)
+
+    if (loading) return <div>loaidng...</div>
+
+    if (error) return <div>Some error happen</div>
+
+
 
     return (
         <div className="MainHero">
             <Container>
-                    <div className="header-container">
-                        <h2>Find your <br/> new four-legged <br/> best friend</h2>
-                        <img src={animals.rhino} />
-                    </div>
-                    <div className="cards-container">
-                        {[].map(card => {
-                            return (
-                                <div className="card">
-                                    <h3>{card.title}</h3>
-                                    <img src={animals[card.img]} style={{width: "100%"}}/>
-                                </div>
-                            )
-                        })}
-                    </div>
+                <div className="header-container">
+                    <h2>Find your <br /> new four-legged <br /> best friend</h2>
+                    <img src={animals.rhino} />
+                </div>
+                <div className="cards-container">
+                    {data.mainCards.map(card => {
+                        return (
+                            <div key={card.title} className="card">
+                                <h3>{card.title}</h3>
+                                <img src={animals[card.image]} style={{ width: "100%" }} />
+                            </div>
+                        )
+                    })}
+                </div>
             </Container>
         </div>
     )
