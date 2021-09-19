1f4b4276344412baba0c2f63de57333b22ee13b2
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-19 01:40:23 -0700

    update frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,18 @@ import { Switch, Route, BrowserRouter } from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import CategoryPage from './pages/CategoryPage';
 import AnimalPage from './pages/AnimalPage/AnimalPage';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route exact strict path="/" component={LandingPage}/>
-            <Route exact strict path="/products/:slug" component={CategoryPage}/>
-            <Route exact strict path="/product/:slug" component={AnimalPage}/>
-          </Switch>
+        <Nav />
+        <Switch>
+          <Route exact strict path="/" component={LandingPage} />
+          <Route exact strict path="/products/:slug" component={CategoryPage} />
+          <Route exact strict path="/product/:slug" component={AnimalPage} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
